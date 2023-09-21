ed4e2324a2abc79e2ac7b3d70c4258e7fbb5fe4e
ning2510 <1367560112@qq.com>
2023-09-21 22:35:31 +0800

    Using threads finish

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t mutex[NBUCKET];
 
 double
 now()
@@ -46,6 +47,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&mutex[i]);
   if(e){
     // update the existing key.
     e->value = value;
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&mutex[i]);
 }
 
 static struct entry*
@@ -62,9 +65,11 @@ get(int key)
 
 
   struct entry *e = 0;
+  // pthread_mutex_lock(&mutex[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  // pthread_mutex_unlock(&mutex[i]);
 
   return e;
 }
@@ -115,6 +120,10 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  for(int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&mutex[i], NULL);
+  }
+
   //
   // first the puts
   //
@@ -144,4 +153,8 @@ main(int argc, char *argv[])
 
   printf("%d gets, %.3f seconds, %.0f gets/second\n",
          NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+
+  // for(int i = 0; i < NBUCKET; i++) {
+  //   pthread_mutex_destroy(&mutex[i]);
+  // }
 }

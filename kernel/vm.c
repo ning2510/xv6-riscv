c85d49a0e8517b115b123a1788af50b0b9c0b20d
ning2510 <1367560112@qq.com>
2024-03-27 22:43:05 +0800

    docs: update docs

--- kernel/vm.c
+++ kernel/vm.c
@@ -170,7 +170,8 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
     if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+      continue;
+      // panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
       continue;
       // panic("uvmunmap: not mapped");
@@ -305,7 +306,8 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
-      panic("uvmcopy: pte should exist");
+      continue;
+      // panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       continue;
       // panic("uvmcopy: page not present");
